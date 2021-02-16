b36819d5e88bf95b143500a07e51344d5b937462
Wallace Gonçalves <wallacecosme@gmail.com>
2021-02-15 22:13:59 -0300

    Implementando paginação no serviço

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,7 +2,11 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+//Com essa anotação (@EnableSpringDataWebSupport) habilitamos esse suporte, para o Spring pegar da requisição, dos parâmetros da url os campos, 
+//as informações de paginação e ordenação, e repassar isso para o Spring data.
+@EnableSpringDataWebSupport 
 @SpringBootApplication
 public class ForumApplication {
 
--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,8 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,10 +41,8 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd){
-		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
-		
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao){
+						
 		Page<Topico> topicos = null;
 		
 		if(nomeCurso == null) {
