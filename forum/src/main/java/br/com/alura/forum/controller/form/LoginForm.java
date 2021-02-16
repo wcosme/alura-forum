d9aecd59bf07cdac06a64fafaa1360140150072d
Wallace Gonçalves <wallacecosme@gmail.com>
2021-02-16 19:20:07 -0300

    Implementando Token

--- forum/src/main/java/br/com/alura/forum/controller/form/LoginForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
 	
 	private String email;
@@ -17,6 +19,10 @@ public class LoginForm {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public UsernamePasswordAuthenticationToken converter() {
+		
+		return new UsernamePasswordAuthenticationToken(email, senha);
 	}	
 
 }
