e2ac765a2c21ced88ff1c18a4a176e53db21e655
Wallace Gonçalves <wallacecosme@gmail.com>
2021-02-18 22:24:43 -0300

    Monitoramento com Spring Boot Actuator

--- forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
