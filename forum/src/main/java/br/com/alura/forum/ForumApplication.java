6453e872320c76a0ee3a45c1b0e4eb53af661c15
Wallace Gonçalves <wallacecosme@gmail.com>
2021-03-13 17:18:18 -0300

    Alterando as configurações para gerar um arquivo WAR

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,6 +2,8 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -14,10 +16,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //habilita o uso de cache na aplicação.
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication {
+public class ForumApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApplication.class);
 	}
 
 }
