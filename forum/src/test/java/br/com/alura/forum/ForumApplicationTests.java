4adab1373c0c036313a5ad66ac1cf3f8f2d928b1
Wallace Gonçalves <wallacecosme@gmail.com>
2021-09-09 16:18:57 -0300

    Junit5

--- forum/src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ forum/src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -2,11 +2,8 @@ package br.com.alura.forum;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumApplicationTests {
 
